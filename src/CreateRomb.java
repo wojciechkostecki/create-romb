b2e50c033e32d25a0edd2f498792920cf727f9a1
Wojciech Kostecki <wt.kostecki@gmail.com>
2020-04-10 23:21:55 +0200

    Print a diamond whose side consists of stars in the amount given by the user

--- src/CreateRomb.java
+++ src/CreateRomb.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class CreateRomb {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Wprowadź liczbę całkowitą: ");
+        int userNumber = scanner.nextInt();
+        int i, j;
+
+        for (i = 1; i <= userNumber; i++) {
+            for (j = 1; j <= userNumber - i; j++) {
+                System.out.print(" ");
+            }
+            for (j = 1; j <= userNumber; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
