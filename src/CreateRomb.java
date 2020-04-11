e46ba397e7e93660d245786f90377cd7cd02f63f
Wojciech Kostecki <wt.kostecki@gmail.com>
2020-04-11 19:51:49 +0200

    Change of user command

--- src/CreateRomb.java
+++ src/CreateRomb.java
@@ -3,15 +3,14 @@ import java.util.Scanner;
 public class CreateRomb {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Wprowadź liczbę całkowitą: ");
-        int userNumber = scanner.nextInt();
-        int i, j;
+        System.out.println("Wprowadź liczbę, stanowiącą bok rombu: ");
+        int sideRomb = scanner.nextInt();
 
-        for (i = 1; i <= userNumber; i++) {
-            for (j = 1; j <= userNumber - i; j++) {
+        for (int i = 1; i <= sideRomb; i++) {
+            for (int j = 1; j <= sideRomb - i; j++) {
                 System.out.print(" ");
             }
-            for (j = 1; j <= userNumber; j++) {
+            for (int j = 1; j <= sideRomb; j++) {
                 System.out.print("*");
             }
             System.out.println();
